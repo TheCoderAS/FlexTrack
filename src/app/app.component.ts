1fb26283764e003643a18f05c7b1cd1007edc673
TheCoderAS <aaloksah766626@gmail.com>
2024-04-09 11:44:56 +0530

    Basics of angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'workout-tracker';
+  title = 'Welcome';
+  count=0;
+  changeTitle(event:MouseEvent):void{
+    console.log(event.target);
+    this.title="Clicked "+(this.count++);
+  };
 }
