878782b68f9fb776efa4dd0a18eeb97ce73eb8c9
TheCoderAS <aaloksah766626@gmail.com>
2024-09-21 03:04:48 +0530

    URL rewrites added with 404 age

--- dist/workout-tracker/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{b as a}from"./chunk-HQFD3LRI.mjs";import"./chunk-RZNRPJOG.mjs";import"./chunk-ZZGUZDFG.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
