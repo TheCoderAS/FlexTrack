7ee42323ca24c2143bc010a54bba9e35565d4255
TheCoderAS <aaloksah766626@gmail.com>
2024-09-22 17:41:59 +0530

    Project restructuring

--- src/main.server.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/components/app/app.component';
-import { config } from './app/components/app/app.config.server';
-import 'localstorage-polyfill';
-
-global['localStorage']=localStorage;
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
