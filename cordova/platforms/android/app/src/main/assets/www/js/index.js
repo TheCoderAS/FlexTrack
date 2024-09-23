ba7cf31a271074514c307f7d458e14a85c2461ed
TheCoderAS <aaloksah766626@gmail.com>
2024-09-23 11:03:50 +0530

    Build sourcemap removed

--- cordova/platforms/android/app/src/main/assets/www/js/index.js
+++ cordova/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,9 +1,30 @@
-function init() {
+async function init() {
   console.log("Welcome to FlexTrack");
-  let script = document.createElement('script');
-    script.setAttribute('src', 'js/dynamic.js');
-    let app = document.getElementById('app');
-    app.appendChild(script);
+  const parser = new DOMParser();
+  try {
+    let response = await fetch('https://flextrack-vibx.onrender.com');
+    let remote = await response.text();
+
+    let remoteDoc = parser.parseFromString(remote, 'text/html');
+
+    let scripts = remoteDoc.getElementsByTagName('script');
+
+    console.log(scripts);
+  } catch (error) {
+    console.log("Cannot load remote UI.", error)
+  }
+  // let app = document.getElementById('app');
+
+  // let script1 = document.createElement("script");
+  // script1.setAttribute('src', 'https://flextrack-vibx.onrender.com/polyfills-Q763KACN.js');
+  // script1.setAttribute('type', 'module');
+
+  // let script2 = document.createElement("script");
+  // script2.setAttribute('src', 'https://flextrack-vibx.onrender.com/main-FRSVORSD.js');
+  // script2.setAttribute('type', 'module');
+
+  // app.appendChild(script1);
+  // app.appendChild(script2);
 }
 document.addEventListener('deviceready', function () {
   init();
